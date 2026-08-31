a22bccaf6f4f9b8bd48ab1cad229336b4a935ad5
rmd352268-dev <rmd352268@gmail.com>
2026-09-01 01:53:45 +0600

    Auto sync: app/Providers/AppServiceProvider.php [2026-09-01 01:53:45]

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,12 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production') ||
-            (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') ||
-            (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ||
-            str_starts_with((string) config('app.url'), 'https://')
-        ) {
-            URL::forceScheme('https');
-        }
+        //
     }
 }
+
