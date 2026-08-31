ee54b305a6f273106e8f86500901c99bcd8d21bb
rmd352268-dev <rmd352268@gmail.com>
2026-09-01 00:05:01 +0600

    Auto sync: cloudflared.exe, install_service.bat, start_tunnel.bat [2026-09-01 00:05:01]

--- install_service.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: Batch script to install Cloudflare Tunnel as a permanent Windows Service (Run as Administrator)
-echo Installing Cloudflare Tunnel as Windows Service...
-"C:\Users\hp\cloudflared.exe" service install eyJhIjoiN2M1YjJmZGZmZmNjZWEzYmFhOTg3YjUzZGFlYmE4MDQiLCJ0IjoiYjc0MTE3ODYtN2MzNy00Njk5LWI0ZjEtYjcyMTExYWY1ZDBlIiwicyI6Ik1qZzNNV1prWXpZdE1HWmpPQzAwWmpnd0xXRXpNalV0TVdSalltSmtNalkzWW1JNSJ9
-echo.
-echo Service Installed successfully! Starting service...
-net start cloudflared
-pause
