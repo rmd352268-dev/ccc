e5f19d41570ee79a8481f59ca8c9e74fa94f0ced
rmd352268-dev <rmd352268@gmail.com>
2026-08-31 23:36:49 +0600

    Auto sync: install_service.bat, start_tunnel.bat [2026-08-31 23:36:49]

---
+++ start_tunnel.bat
@@ -0,0 +1,7 @@
+@echo off
+title Cloudflare Tunnel Runner
+echo ========================================================
+echo Starting Cloudflare Tunnel connected to your Zero Trust!
+echo ========================================================
+"C:\Users\hp\cloudflared.exe" tunnel run --token eyJhIjoiN2M1YjJmZGZmZmNjZWEzYmFhOTg3YjUzZGFlYmE4MDQiLCJ0IjoiYjc0MTE3ODYtN2MzNy00Njk5LWI0ZjEtYjcyMTExYWY1ZDBlIiwicyI6Ik1qZzNNV1prWXpZdE1HWmpPQzAwWmpnd0xXRXpNalV0TVdSalltSmtNalkzWW1JNSJ9
+pause
